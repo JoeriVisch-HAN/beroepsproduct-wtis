3375bd6d1346b96e640e97fb32d5884e4f33db3a
JoeriVisch-HAN <JJA.Visch@student.han.nl>
2022-12-19 16:17:06 +0100

    database settings

--- applicatie/index.php
+++ applicatie/index.php
@@ -1,13 +1,19 @@
-<?php 
+<?php
 include("./components/header.html");
+include('db_connectie.php');
+$conn = maakVerbinding();
+
+$sql = 'select * from Gate';
+$stmt = $conn->query($sql);
+
 ?>
 
 <div class="fotohome">
-        <img src="images/3840x2160a.jpg" alt="vliegtuig">
-        <h1>Gelre Airport</h1>
-        <p>Het mooiste vliegveld uit gelderland</p>
-      </div>
+    <img src="images/3840x2160a.jpg" alt="vliegtuig">
+    <h1>Gelre Airport</h1>
+    <p>Het mooiste vliegveld uit gelderland</p>
+</div>
 
-<?php 
+<?php
 include("./components/footer.html");
-?>      
+?>
--- applicatie/inloggen.php
+++ applicatie/inloggen.php
@@ -3,25 +3,25 @@ include("./components/header.html");
 ?>
 
 <form action="#" method="post">
-            <h1>login</h1>
-            <p>Log hier in om verder te kunnen op de website!</p>
-            <label>
-                passagier of medewerker?
-                <select>
-                    <option>passagier</option>
-                    <option>medewerker</option>
-                </select>
-            </label>
-            <label>
-                username:
-                <input type="text" name="username" pattern="[a-zA-Z]+" required>
-            </label>
-            <label>
-                password:
-                <input type="password" name="password" minlength="5" required>
-            </label>
-            <label>
-                inloggen:
-                <input type="submit" name="inloggen" value="inloggen">
-            </label>
-        </form>
+    <h1>login</h1>
+    <p>Log hier in om verder te kunnen op de website!</p>
+    <label>
+        passagier of medewerker?
+        <select>
+            <option>passagier</option>
+            <option>medewerker</option>
+        </select>
+    </label>
+    <label>
+        username:
+        <input type="text" name="username" pattern="[a-zA-Z]+" required>
+    </label>
+    <label>
+        password:
+        <input type="password" name="password" minlength="5" required>
+    </label>
+    <label>
+        inloggen:
+        <input type="submit" name="inloggen" value="inloggen">
+    </label>
+</form>
