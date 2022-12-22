23884136f3a321af344ea657e23b75ffa837a5d0
JoeriVisch-HAN <JJA.Visch@student.han.nl>
2022-12-22 09:16:30 +0100

    links gemaakt

--- applicatie/medewerker.php
+++ applicatie/medewerker.php
@@ -5,26 +5,26 @@ include("./components/header.html");
     <h1>medewerker</h1>
     <article>
         <article>
-            <a href="bagage_inchecken_medewerker.html">
+            <a href="bagage_inchecken_medewerker.php">
                 <img src="images/luggage-g70559e9e0_640.jpg" alt="koffer">
                 <h2>bagage inchecken</h2>
             </a>
         </article>
         <article>
-            <a href="vluchtgegevens.html">
+            <a href="vluchtgegevens.php">
                 <img src="images/airport-g7cf5ac3ef_640.jpg" alt="vluchtgegevens">
                 <h2>vluchtgegevens bekijken</h2>
             </a>
 
         </article>
         <article>
-            <a href="vlucht_invoeren.html">
+            <a href="vlucht_invoeren.php">
                 <img src="images/3840x2160a.jpg" alt="koffer">
                 <h2>vlucht toevoegen</h2>
             </a>
         </article>
         <article>
-            <a href="passagier_toevoegen.html">
+            <a href="passagier_toevoegen.php">
                 <img src="images/silhouette-g39fec6e65_640.jpg" alt="passagier">
                 <h2>passagier toevoegen</h2>
             </a>
--- applicatie/passagier.php
+++ applicatie/passagier.php
@@ -5,14 +5,14 @@ include("./components/header.html");
             <h1>passagier</h1>
             <article>
                 <article>
-                    <a href="bagage_inchecken.html">
+                    <a href="bagage_inchecken.php">
                     <img src="images/luggage-g70559e9e0_640.jpg" alt="koffer">
                     
                         <h2>bagage inchecken</h2>
                     </a>
                 </article>
                 <article>
-                    <a href="vluchtgegevens_klant.html">
+                    <a href="vluchtgegevens_klant.php">
                     <img src="images/airport-g7cf5ac3ef_640.jpg" alt="vluchtgegevens">
                     
                         <h2>vluchtgegevens bekijken</h2>
